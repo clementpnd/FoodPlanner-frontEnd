b759db720ce15d1202933b6a7da2e8e5080672c1
Anthony <anthonyanadi@gmail.com>
2023-05-15 11:48:43 +0200

    food

--- App.js
+++ App.js
@@ -5,6 +5,20 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import user from "./reducers/user";
+//import storage from "redux-persits/lib/storage";
+
+const reducers = combineReducers({ user });
+const persistConfig = { key: "users" };
+
+const store = configureStore({
+  reducer: persitsReducer(persistConfig, reducers),
+  middleware: (getDefaultMiddleware: any) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+});
+
 import AccueilScreen from "./screens/AccueilScreen";
 import LandingPageScreen from "./screens/LandingPageScreen";
 import FavorisScreen from "./screens/FavorisScreen";
@@ -18,7 +32,11 @@ const Tab = createBottomTabNavigator();
 //heart : heart-o/
 //Home : home
 //
-styleTabBar = { backgroundColor: "#E4631B", borderTopLeftRadius: 8, borderTopRightRadius:8 };
+styleTabBar = {
+  backgroundColor: "#E4631B",
+  borderTopLeftRadius: 8,
+  borderTopRightRadius: 8,
+};
 const TabNavigator = () => {
   return (
     <Tab.Navigator
@@ -35,13 +53,13 @@ const TabNavigator = () => {
           }
           return <FontAwesome name={iconName} size={size} color={color} />;
         },
-        tabBarActiveTintColor: "#FDFEFE" ,
+        tabBarActiveTintColor: "#FDFEFE",
         tabBarInactiveTintColor: "#979A9A",
         headerShown: false,
-        tabBarLabelStyle : {color : "white"},
+        tabBarLabelStyle: { color: "white" },
         tabBarStyle: styleTabBar,
         initialRouteName: "Ma Semaine",
-        tabBarBadgeStyle : {backgroundColor : "red"}
+        tabBarBadgeStyle: { backgroundColor: "red" },
       })}
     >
       <Tab.Screen name="Favoris" component={FavorisScreen} />
@@ -52,12 +70,19 @@ const TabNavigator = () => {
 };
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screensOption={{ headerShown: false }}>
-        <Stack.Screen name="LandingPageScreen" component={LandingPageScreen} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-        <Tab.Screen name="AccueilScreen" component={AccueilScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator screensOption={{ headerShown: false }}>
+            <Stack.Screen
+              name="LandingPageScreen"
+              component={LandingPageScreen}
+            />
+            <Stack.Screen name="TabNavigator" component={TabNavigator} />
+            <Tab.Screen name="AccueilScreen" component={AccueilScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
+    </Provider>
   );
 }
