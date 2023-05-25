1fbcf2714d684d7d0765f44565836fad66727d3e
Lempereur <lempereur.julien83@gmail.com>
2023-05-25 12:17:07 +0200

    s

--- screens/ProfilsScreen.js
+++ screens/ProfilsScreen.js
@@ -8,6 +8,8 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  ScrollView,
+  Switch,
 } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
@@ -20,9 +22,8 @@ export default function ProfilsScreen({ navigation }) {
   const [mail, setMail] = useState("");
   const [mdp, setMdp] = useState();
   const [editable, setEditable] = useState(false);
-  const dispatch  =useDispatch();
+  const dispatch = useDispatch();
   const user = useSelector((state) => state.users.value);
-
 
   useEffect(() => {
     fetch(`http://10.2.1.12:3000/users/${user.token}`)
@@ -32,15 +33,14 @@ export default function ProfilsScreen({ navigation }) {
         setPseudo(data.user.pseudo);
         setMail(data.user.mail);
         if (data.user.photoProfil === "") {
-          image = require("../assets/User.png")
-        }
-        else{
+          image = require("../assets/User.png");
+        } else {
           setPhotoProfil(data.user.photoProfil);
         }
       });
   }, [photoProfil]);
-  
-let image = {uri : photoProfil};
+
+  let image = { uri: photoProfil };
   if (user.photoProfil !== undefined) {
     image = { uri: user.photoProfil };
     if (!user.photoProfil) {
@@ -48,81 +48,125 @@ let image = {uri : photoProfil};
     }
   }
 
+  const logout = () => {
+    dispatch(removeUsers());
+    navigation.navigate(" ");
+  };
 
-const logout =() =>{
-dispatch(removeUsers());
-navigation.navigate(" ")
-}
-
-const saveData =() =>{
-  setEditable(!editable);
-  fetch(`http://10.2.1.12:3000/users/profilUpdate/${user.token}`, {
+  const saveData = () => {
+    setEditable(!editable);
+    fetch(`http://10.2.1.12:3000/users/profilUpdate/${user.token}`, {
       method: "PUT",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-         prenom : prenom,
-         pseudo : pseudo,
-         photoProfil : user.photoProfil,
-       }),
+        prenom: prenom,
+        pseudo: pseudo,
+        photoProfil: user.photoProfil,
+      }),
     })
-    .then(response=>response.json())
-    .then((data) =>{
-      if(data.result ===false){
-        console.warn(data.error);
-      }
-    }) 
-}
-
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.result === false) {
+          console.warn(data.error);
+        }
+      });
+  };
 
   let colorFont = "black";
-  let buttonSubmit ;
+  let buttonSubmit;
   let buttonPhoto;
 
   if (editable) {
-    colorFont = "red";
-    buttonSubmit = 
-      <View><TouchableOpacity style={styles.saveButton} onPress={() => saveData()}>
-          <Text style={styles.picturesText} >Save</Text>
+    colorFont = "#E4631B";
+    buttonSubmit = (
+      <View>
+        <TouchableOpacity style={styles.saveButton} onPress={() => saveData()}>
+          <Text style={styles.picturesText}>Save</Text>
         </TouchableOpacity>
-        </View>
-      
-    buttonPhoto = 
-    <TouchableOpacity style={styles.pictureButton} onPress={() => navigation.navigate("CameraScreen")}>
-          <Text style={styles.picturesText}>Photo de profil</Text>
-        </TouchableOpacity>
+      </View>
+    );
+
+    buttonPhoto = (
+      <TouchableOpacity
+        style={styles.pictureButton}
+        onPress={() => navigation.navigate("CameraScreen")}
+      >
+        <Text style={styles.picturesText}>Photo de profil</Text>
+      </TouchableOpacity>
+    );
   }
 
-
   return (
-    <View style={styles.profilDiv}>
-      <View style={styles.textProfileDiv}>
-        <Text style={styles.textProfile}>Profile</Text>
-        <Button title="Logout" onPress={() =>logout()}/>
-      </View>
-      <View style={styles.photoDiv}>
-        <Image source={image} style={styles.img} />
-        {buttonPhoto}
-        
-      </View>
-      <View style={styles.AllTextInfoDiv}>
-        <TouchableOpacity onPress={() => setEditable(!editable)}>
-          <FontAwesome name="pencil" size={40} color={colorFont} />
-        </TouchableOpacity>
-        <View style={[styles.textPrenomDiv, styles.divText]}>
-          <TextInput value={prenom} editable={editable} style={styles.text} onChangeText={(e) => setPrenom(e)}/>
+    <ScrollView>
+      <View style={styles.profilDiv}>
+        <View style={styles.titleDiv}>
+          <Text style={styles.textProfile}>Profile</Text>
         </View>
-        <View style={[styles.textPseudoDiv, styles.divText]}>
-          <TextInput value={pseudo} editable={editable} style={styles.text} onChangeText={(e) => setPseudo(e)}/>
+        <View style={styles.logout}>
+          <TouchableOpacity
+            style={styles.pictureButton}
+            onPress={() => logout()}
+          >
+            <Text style={styles.picturesText}>Logout</Text>
+          </TouchableOpacity>
         </View>
-        <View style={[styles.textMailDiv, styles.divText]}>
-          <TextInput value={mail} editable={false} style={styles.text} />
+        <View style={styles.photoDiv}>
+          <Image source={image} style={styles.img} />
+          {buttonPhoto}
         </View>
+        <View style={styles.AllTextInfoDiv}>
+          <TouchableOpacity onPress={() => setEditable(!editable)}>
+            <FontAwesome name="pencil" size={40} color={colorFont} />
+          </TouchableOpacity>
+          <View style={[styles.textPrenomDiv, styles.divText]}>
+            <TextInput
+              value={prenom}
+              editable={editable}
+              style={styles.text}
+              onChangeText={(e) => setPrenom(e)}
+            />
+          </View>
+          <View style={[styles.textPseudoDiv, styles.divText]}>
+            <TextInput
+              value={pseudo}
+              editable={editable}
+              style={styles.text}
+              onChangeText={(e) => setPseudo(e)}
+            />
+          </View>
+          <View style={[styles.textMailDiv, styles.divText]}>
+            <TextInput value={mail} editable={false} style={styles.text} />
+          </View>
+          <View style={styles.preferenceDiv}>
+            <View>
+              <Text style={styles.preferenceTitle}>Vos Preference</Text>
+            </View>
+            <View style={styles.switchDiv}>
+              <View style={styles.regime}>
+                <Text>Végé</Text>
+                <Switch
+                  value={true}
+                  // disabled={aucun}
+                />
+              </View>
+              <View style={styles.regime}>
+              <Text>poulet</Text>
+              <Switch value={true} />
+            </View>
+            <View style={styles.regime}>
+              <Text>poisson</Text>
+              <Switch value={true} />
+            </View>
+            <View style={styles.regime}>
+              <Text>feculent</Text>
+              <Switch value={true} />
+            </View>
+            </View>
+          </View>
+        </View>
+        <View>{buttonSubmit}</View>
       </View>
-      <View>
-        {buttonSubmit}
-      </View>
-      
-    </View>
+    </ScrollView>
   );
 }
 
@@ -132,8 +176,10 @@ const styles = StyleSheet.create({
     width: Dimensions.get("window").width,
     height: Dimensions.get("window").height,
     alignItems: "center",
+    marginBottom: 140,
   },
-  textProfileDiv: {
+  titleDiv: {},
+  logout: {
     marginBottom: 40,
   },
   textProfile: {
@@ -142,7 +188,7 @@ const styles = StyleSheet.create({
   },
   photoDiv: {
     flex: 1,
-    alignItems : "center",
+    alignItems: "center",
   },
   img: {
     width: 200,
@@ -176,7 +222,15 @@ const styles = StyleSheet.create({
   picturesText: {
     fontSize: 20,
   },
-  saveButton:{
+
+  preferenceDiv: {
+    alignItems: "center",
+  },
+  preferenceTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  saveButton: {
     marginTop: 10,
     width: 130,
     height: 40,
@@ -184,5 +238,16 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     backgroundColor: "rgba(228, 255, 27, 0.6)",
     borderRadius: 10,
-  }
+  },
+  switchDiv: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between"
+  //   alignItems: "flex-start",
+  //   width: Dimensions.get("window").width,
+  //   height: 500,
+  //   marginTop: 20,
+  //   backgroundColor: "red",
+  },
 });
